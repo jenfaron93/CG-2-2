53f5a551771c05d28a91832ce7bc0a9319236b99
Jennifer Faron <jf891w@gmail.com>
2018-07-15 20:11:39 -0500

    Area of a rectangle

---
+++ RectangleArea/RectangleArea/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RectangleArea
+{
+    class Program
+    {
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Area of a rectangle");
+			Console.Write("Please enter a length: ");
+			string length = Console.ReadLine();
+
+			Console.Write("Please enter a width: ");
+			string width = Console.ReadLine();
+
+			int area = Convert.ToInt32(length) * Convert.ToInt32(width);
+
+			Console.WriteLine("The area of your rectangle is " + area + ".");
+			Console.ReadLine();
+
+
+
+
+        }
+    }
+}
